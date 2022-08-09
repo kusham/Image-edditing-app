932b386ede5d48213404b563434dc6f30026c079
kusham <kusham1998@gmail.com>
2022-08-10 03:50:24 +0530

    add navbar

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './NavBarStyle.css'
+
+const NavBar = () => {
+  return (
+    <div className='navBar'>
+        <ion-icon name="menu-outline"></ion-icon>
+        <div >
+        <p>Image Editor</p>
+
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
